d0c90aa7bea3ea2f3610f710dd2c6b31fe035a0a
= <=>
2025-04-11 12:04:48 +0530

    Unique Elements of array

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
-import oops.ClassMethod;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 
 public class Main{
+
+        public static Long convertToTimeStamp(LocalDateTime date)
+    {
+        ZoneId zoneId = ZoneId.of("Asia/Kolkata");
+        return date.atZone(zoneId).toInstant().toEpochMilli();
+    }
+
+    public static String convertTimestampToDate(Long timestamp)
+    {
+        ZoneId zoneId = ZoneId.of("Asia/Kolkata");
+        LocalDateTime localDateTime = Instant.ofEpochMilli(timestamp).atZone(zoneId.systemDefault()).toLocalDateTime();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String ltdstr = localDateTime.format(dateTimeFormatter);
+        return ltdstr;
+    }
+
     public static void main(String[] args){
-        ClassMethod classMethod = new ClassMethod(24,22);
-//        int c = classMethod.add();
-//        System.out.println("Sum = "+c);
+        LocalDateTime dateinput = LocalDateTime.now();
+        Long epochTime = convertToTimeStamp(dateinput);
+        System.out.println(epochTime);
+        String dateTime = convertTimestampToDate(epochTime);
+        System.out.println(dateTime);
     }
 }
---
+++ src/arrayexcercise/UniqueElement.java
@@ -0,0 +1,49 @@
+package arrayexcercise;
+
+import java.util.Scanner;
+
+public class UniqueElement {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size: ");
+        int num = sc.nextInt();
+
+        int[] arr;
+        arr = new int[num];
+
+        System.out.println("Enter array: ");
+        for(int i=0; i<num; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean flag = false;
+
+        System.out.println("Print array");
+
+        System.out.println("Unique elements of array");
+
+        for(int j=0; j<num; j++)
+        {
+            flag = false;
+            for(int k=0; k<num; k++)
+            {
+                if(arr[j] == arr[k] && j != k ) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(flag == false)
+            {
+                System.out.println(arr[j]);
+            }
+
+        }
+
+
+        sc.close();
+    }
+}
